fa9ccd6bf73d528e1210c1db22c603b7eaaa66a5
Prof. Marlon <marlonbsi@protonmail.com>
2025-09-17 21:01:57 -0300

    Adicionando Tomate na linha 14

--- aula04/aula2008/Main.java
+++ aula04/aula2008/Main.java
@@ -34,6 +34,7 @@ public class Main {
 		coisas.add("Banana");
 		coisas.add("Batata");
 		coisas.add("Maçã");
+		coisas.add("Alface");
 		System.out.println(coisas.contains("Bacana"));
 		
 		System.out.println();
